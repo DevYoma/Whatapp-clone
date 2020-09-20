946ce04135714a370c6a25eb0c4329e304e0bda4
DevYoma <Lawrenceyoma@gmail.com>
2020-09-21 00:13:43 +0100

    almost completed the sidebar

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC7CcZDvhsMS_rKzoP6urVKQQKhsdHz1qI",
+    authDomain: "whatsapp-clone-70a40.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-70a40.firebaseio.com",
+    projectId: "whatsapp-clone-70a40",
+    storageBucket: "whatsapp-clone-70a40.appspot.com",
+    messagingSenderId: "671731503011",
+    appId: "1:671731503011:web:828f660f0c66654dbe1416",
+    measurementId: "G-0TEPKDH0FY"
+  };
+
+  
